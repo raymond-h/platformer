fe7e5b08b47e5926362531bc2ab2b143965ef93f
Raymond Hammarling <raykay.rh@gmail.com>
2014-03-10 23:08:37 +0100

    Added AnimationManager class

--- src/Animation.cpp
+++ src/Animation.cpp
@@ -18,10 +18,22 @@ std::string toString(std::istream& in) {
 	return str;
 }
 
-AnimationManager::AnimationManager(boost::filesystem::path file):w(0),h(0) {
+Frame parseJsonFrame(JSONNode node) {
+	Frame f;
+	f.x = node["x"].as_int();
+	f.y = node["y"].as_int();
+	f.duration = node["duration"].as_int();
+	return f;
+}
+
+AnimationManager::AnimationManager(boost::filesystem::path file) : _counter(0),_currentAnimation(""),
+		_currentFrame(0),w(0),h(0),image(),animations() {
+
 	boost::filesystem::ifstream fin(file);
 
+
 	parseJson( toString(fin) );
+	reset();
 }
 
 void AnimationManager::parseJson(std::string jsonStr) {
@@ -31,10 +43,50 @@ void AnimationManager::parseJson(std::string jsonStr) {
 	h = root["height"].as_int();
 
 	std::string imagePath = root["image"].as_string();
+
 	SurfacePtr tmp = wrap( IMG_Load(imagePath.c_str()) );
+
 	image = createFromSurface(Game::renderer(), tmp);
 
 	for(JSONNode animation : root["animations"]) {
-		std::cout << "Got animation: " << animation.name() << "; " << animation.write() << std::endl;
+		// std::cout << "Got animation: " << animation.name() << "; " << animation.write() << std::endl;
+		for(JSONNode frame : animation) {
+			animations[animation.name()].push_back(parseJsonFrame(frame));
+		}
 	}
+}
+
+void AnimationManager::update(unsigned long delta) {
+	// std::cout << "Updating with delta " << delta << std::endl;
+
+	while(_counter < delta) {
+		// std::cout << "inf loop" << std::endl;
+		unsigned long oldCounter = _counter;
+		progressFrame();
+		delta -= oldCounter;
+
+		if(_counter == 0) break;
+	}
+	_counter -= delta;
+}
+
+void AnimationManager::progressFrame() {
+	const std::vector<Frame>& frames = animations[_currentAnimation];
+
+	_currentFrame++;
+	if(_currentFrame >= frames.size()) _currentFrame = 0;
+
+	_counter = frames[_currentFrame].duration;
+}
+
+void AnimationManager::setAnimation(std::string animation) {
+	if(animation != _currentAnimation) {
+		_currentAnimation = animation;
+		reset();
+	}
+}
+
+void AnimationManager::reset() {
+	_currentFrame = 0;
+	_counter = animations[_currentAnimation].size() > 0 ? animations[_currentAnimation][0].duration : 0;
 }
--- src/Animation.h
+++ src/Animation.h
@@ -1,4 +1,6 @@
 #include <string>
+#include <map>
+#include <vector>
 
 #include <boost/filesystem.hpp>
 
@@ -22,11 +24,25 @@ public:
 
 	void update(unsigned long delta);
 
+	void setAnimation(std::string animation);
+
+	void reset();
+
+	SDL_Rect getArea() { Frame f = animations[_currentAnimation][_currentFrame]; return { f.x, f.y, w, h }; };
+	TexturePtr texture() { return image; };
+
+private:
+	void progressFrame();
+
 private:
 	unsigned long _counter;
+	std::string _currentAnimation;
+	unsigned long _currentFrame;
 
 	unsigned long w, h;
 	TexturePtr image;
+
+	std::map< std::string, std::vector< Frame > > animations;
 };
 
 #endif /* ANIMATION_H_ */
